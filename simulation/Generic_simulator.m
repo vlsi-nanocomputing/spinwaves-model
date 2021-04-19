4dc58d477169cd58cd5bb7fda6ae0ce5bb9a4d6b
Huqing Zheng <s266518@studenti.polito.it>
2021-04-19 10:04:32 +0200

    I insert LineWidth parameter in Generic_simulator.m to set the LineWidth of all the lines

--- simulation/Generic_simulator.m
+++ simulation/Generic_simulator.m
@@ -33,11 +33,12 @@ C_bin = [1];
 D_bin = [0];
 
 Nbit = 2;   % it is used by RCA and CSA (parallelism). For the Carry-Skip Adder, the Nbit must be a multiple of 4, which is a constraint of the CSA model
-opt_parameters = {'HA_without_regS','HA_without_regC','out_signal_plot'}; % optional parameters, it can be empty
+opt_parameters = {'out_signal_plot'}; % optional parameters, it can be empty
 titleFontSize = 25;   % title FontSize of the plots
-axisFontSize = 13;     % axes FontSize of the plots
+axisFontSize = 13;    % axes FontSize of the plots
 labelFontSize = 20;   % labels FontSize of the plots
 legendFontSize = 14;  % legend FontSize of the plots
+line_width = 2;       % LineWidth of the lines  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%% Choosing of model category %%%%%%%%%%%%%%%%%%%
@@ -339,7 +340,11 @@ for ii = 1:N_simulation
             analyzed_figures = analyzed_figures + New_figures;
     end
 end
-
+% LineWidth modification
+lines = findobj('type','line');
+for i=1:1:max(size(lines))
+    lines(i).LineWidth = line_width;
+end
 
 
 end % if model~=4
